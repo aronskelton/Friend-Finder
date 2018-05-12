238cc33f6b2fc2471c17977ccdef5c173de41845
aron skelton <aron@skelton.us>
2018-05-12 04:18:52 -0400

    v1

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+var friendsArray = [
+	{
+		name: 'Goku',
+		photo: 'https://vignette.wikia.nocookie.net/dragonball/images/5/5b/Gokusteppingoutofaspaceship.jpg/revision/latest/scale-to-width-down/224?cb=20150325220848',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Vegeta',
+		photo: 'https://vignette.wikia.nocookie.net/warner-bros-characters/images/8/8f/Vegetavwx2014_928.jpg/revision/latest?cb=20160817125149',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Gohan',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Gohan_Lithograph.PNG/250px-Gohan_Lithograph.PNG',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Bulma',
+		photo: 'https://res.cloudinary.com/teepublic/image/private/s--JTcBcgHI--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1482319241/production/designs/976005_1.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	
+module.exports = friendsArray;
